8fb827eef06cdea8a2fce6bd5a1fca236f1e2a09
WeRockStar <vr_freestyle@outlook.com>
2016-10-21 21:00:16 +0700

    Prepare fragment and activity

--- app/src/main/java/com/werockstar/bottomnavigationview/activity/MainActivity.java
+++ app/src/main/java/com/werockstar/bottomnavigationview/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.werockstar.bottomnavigationview;
+package com.werockstar.bottomnavigationview.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.werockstar.bottomnavigationview.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/werockstar/bottomnavigationview/fragment/TwoFragment.java
@@ -0,0 +1,24 @@
+package com.werockstar.bottomnavigationview.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.werockstar.bottomnavigationview.R;
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        return view;
+    }
+
+}
