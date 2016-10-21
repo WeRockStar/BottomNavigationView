74be901ad5f19a837c1702f1263380ee99e33b72
WeRockStar <vr_freestyle@outlook.com>
2016-10-21 21:20:39 +0700

    Prepare implement navigation bottom

--- app/src/main/java/com/werockstar/bottomnavigationview/activity/MainActivity.java
+++ app/src/main/java/com/werockstar/bottomnavigationview/activity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.werockstar.bottomnavigationview.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.werockstar.bottomnavigationview.R;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_bottom);
         setSupportActionBar(toolbar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        
+                        break;
+                    case R.id.menu_settings:
+
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
